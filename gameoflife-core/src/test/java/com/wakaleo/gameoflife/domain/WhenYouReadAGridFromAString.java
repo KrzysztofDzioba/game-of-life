0372a50e5ed600d5d25cf9de51d2ce914759bb60
Krzysztof Dzioba <krzysztof.dzioba.93@gmail.com>
2018-06-13 12:41:30 +0200

    add //

--- gameoflife-core/src/test/java/com/wakaleo/gameoflife/domain/WhenYouReadAGridFromAString.java
+++ gameoflife-core/src/test/java/com/wakaleo/gameoflife/domain/WhenYouReadAGridFromAString.java
@@ -33,6 +33,7 @@ public class WhenYouReadAGridFromAString {
         //
         //
         //
+        //
         Cell[][] expectedCells = {{LIVE_CELL}};
 
         GridReader gridReader = new GridReader();
